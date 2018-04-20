739d2180d9ebd267dbf38a5757ad65388d966ec3
samking257 <samanthajaineking@gmail.com>
2018-04-20 19:58:16 +0100

    Add files via upload

--- ClientServer/src/clientserver/ClientServer.java
+++ ClientServer/src/clientserver/ClientServer.java
@@ -5,9 +5,13 @@
  */
 package clientserver;
 
+import java.io.DataInputStream;
+import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 import systems.software.Message;
 
 /**
@@ -15,7 +19,9 @@ import systems.software.Message;
  * @author Sam King
  */
 public class ClientServer {
-
+    
+    ArrayList<String> list = new ArrayList();
+    ArrayList<Message> Messagelist = new ArrayList();
     /**
      * @param args the command line arguments
      */
@@ -23,7 +29,33 @@ public class ClientServer {
         // TODO code application logic here
     }
     
-    void SendMessage(){
+    void Register()
+    {
+        //List of all members online
+        //User logs in and sends username to the server
+        while(true)
+        {
+            try{
+                //create new server socket
+                ServerSocket SS = new ServerSocket(1010);
+                //Keep checking for message
+                Socket reg = SS.accept();
+                DataInputStream DIS = new DataInputStream(reg.getInputStream());
+                String UserToReg = DIS.readUTF();
+                
+                //Adding username to the register list
+                list.add(UserToReg);
+                                         
+            }catch(Exception E){
+            System.out.println("Network Error:" + E.getMessage());
+            }
+        }
+    }
+    
+    void ReceiveMessage()
+    {   
+        //Message is sent from user to server 
+        //Message is receiced, read and put into a list
         while(true){
             try{
                 //create new server socket
@@ -36,18 +68,42 @@ public class ClientServer {
                 //Read Object as Message
                 Message MessageSending = (Message)OIS.readObject();
                 
-                //Split Object
+                //Add to message array list
+                Messagelist.add(MessageSending);
+                                                           
+            }catch(Exception E){
+                System.out.println("Network Error:" + E.getMessage());
+            }            
+        }    
+    }
+    
+    void sendMessage()
+    {
+        //User will send username to the server
+        //Checks if the user has a message the sends message
+        while(true){
+            try{
+                //Create new Server Socket
+                ServerSocket ss = new ServerSocket(1020);
+                Socket s = ss.accept();                             
+                DataInputStream DIS = new DataInputStream(s.getInputStream());
+                String username = DIS.readUTF();
                 
-                //Find Sender
-                
-                //Send String to Correct User
-                
+                //Loop through messagelist array if username=Receiver send message
+                for(Message M:Messagelist)
+                {
+                    if(username.equals(M.Receiver)){
+                        ObjectOutputStream OOS = new ObjectOutputStream(s.getOutputStream());
+                        Message MessageToSend = new Message();
+
+                        OOS.writeObject(MessageToSend);
+                    } 
+                }
+
             }catch(Exception E){
                 System.out.println("Network Error:" + E.getMessage());
             }
-            
-        }
-    
+         }
     }
     
     
